50730a2e9fd0e2d457fc6e581608ce9552af82c5
Tom Ungerer <tu@tomungerer.com>
2020-07-03 04:00:45 +0100

    Updates

---
+++ precache-manifest.f5b2c6ff37957eebbc009aca5aeb1f33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b801dabdc581bbaf0a55e7bf3e49b5fe",
+    "url": "/windbnb/index.html"
+  },
+  {
+    "revision": "c717d48e6b07139217de",
+    "url": "/windbnb/static/css/main.f2a3e309.chunk.css"
+  },
+  {
+    "revision": "546ceffc1b46689c43cb",
+    "url": "/windbnb/static/js/2.89e699f1.chunk.js"
+  },
+  {
+    "revision": "c3f40239bcd0a13c83dfe66bd9a54414",
+    "url": "/windbnb/static/js/2.89e699f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c717d48e6b07139217de",
+    "url": "/windbnb/static/js/main.ff090b91.chunk.js"
+  },
+  {
+    "revision": "3898273fa5bd4f97b2d8",
+    "url": "/windbnb/static/js/runtime-main.cc1ec942.js"
+  },
+  {
+    "revision": "54ee1f4ab8ad7c2b788a0a594a55db44",
+    "url": "/windbnb/static/media/logo.54ee1f4a.svg"
+  }
+]);
