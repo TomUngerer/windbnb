ae5a8cb96719618f81708620936565be2bc2a34e
Tom Ungerer <tu@tomungerer.com>
2020-07-03 04:00:10 +0100

    first commit

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import Navbar from '../components/layout/Navbar'
+
+import Seo from "../components/layout/Seo"
+import Main from "../components/layout/Main"
+import Section from "../components/layout/Section"
+
+import Properties from "../components/sections/Properties"
+
 import Copyright from '../components/layout/Copyright'
 
 import properties from "../assets/stays.json"
-
-import Home from '../pages/Home'
 
 let cities = [];
 properties.forEach(property => {
@@ -33,6 +38,7 @@ class App extends React.Component {
     const {properties, guests, city} = this.state
     return (
       <div className="App">
+        <Seo title="Home" />
         <div>
           <Navbar />
           <form>
@@ -41,7 +47,17 @@ class App extends React.Component {
           </div>
           Guests : {guests < 11 ? guests : '10+' }
           </form>
-          <Home properties={properties} city={city} limit={guests} />
+          <Main>
+            <Section container="large">
+              <div className="uk-flex uk-flex-bottom uk-flex-between uk-margin-bottom">
+                <h2 className="uk-margin-remove-bottom">Stays in Finland</h2>
+                {properties ? (<div style={{ whiteSpace: "nowrap" }}>
+                  {properties.length < 12 ? properties.length : "12+"} stays
+                </div>) : ''}
+              </div>
+              {properties ? <Properties properties={properties} city={city} limit={guests} /> : ''}
+            </Section>
+          </Main>
           <Copyright />
         </div>
       </div>
